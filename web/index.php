7c70128682c62d16e53187ee596bb56a69e90af6
krishnakamal1223 <51716117+krishnakamal1223@users.noreply.github.com>
2020-08-31 16:24:31 +0530

    Update index.php

--- web/index.php
+++ web/index.php
@@ -2,25 +2,26 @@
 $servername = "localhost";
 $username = "root";
 $password = "kamal";
-$dbname = "kamal";
+$dbname = "hybrid";
+$port = "3306";
 
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $port, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
 die("Connection failed: " . $conn->connect_error);
 }
 
 
-$sql = "SELECT * FROM kamal1";
+$sql = "SELECT * FROM hulk";
 $result = $conn->query($sql);
 
 
 if ($result->num_rows > 0) {
 // output data of each row
 while($row = $result->fetch_assoc()) {
-echo "- Name:" .$row["name"]. " - id: " .$row["id"]. "<br>";
+echo "- Name:" .$row["name"]. " - id: " .$row["id"]. "- salary:" .$row["salary"]. "<br>";
 }
 } else {
 echo "0 results";
