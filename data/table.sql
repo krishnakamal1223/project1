f76383c7e26c52f0dc155eb50b973119186e6e72
krishnakamal <kamalkrishna408@gmail.com>
2020-08-31 06:49:42 -0400

    This is first commit

---
+++ data/table.sql
@@ -0,0 +1,1 @@
+create table hulk(name varchar(20), id integer(10), salary integer(10));
---
+++ data/values.sql
@@ -0,0 +1,6 @@
+insert into hulk values('kamal', 1, 25000);
+insert into hulk values('surya', 2, 25000);
+insert into hulk values('ajay', 3, 25000);
+insert into hulk values('rudra', 4, 25000);
+insert into hulk values('azhar', 5, 25000);
+
