711c914269a388fcf80bdf507739c1fe8ea0eb01
krishnakamal1223 <51716117+krishnakamal1223@users.noreply.github.com>
2020-09-01 14:26:37 +0530

    Update privilages.sql

--- data/privilages.sql
+++ data/privilages.sql
@@ -1,5 +1,5 @@
-GRANT ALL PRIVILEGES ON tech.* TO'root'@'localhost';
-GRANT ALL PRIVILEGES ON tech.* TO'root'@'%';
+GRANT ALL PRIVILEGES ON hybrid.* TO'root'@'localhost';
+GRANT ALL PRIVILEGES ON hybrid.* TO'root'@'%';
 alter user 'root'@'localhost' identified with mysql_native_password by '123';
 alter user 'root'@'%' identified with mysql_native_password by '123';
 
